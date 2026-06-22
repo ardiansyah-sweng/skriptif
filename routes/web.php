e49b82228a365b21866e145a3c86c28f2afe15f5
Project PBO <2300018162@webmail.uad.ac.id>
2026-06-22 08:30:09 +0700

    Menghapus view index dan show skripsi

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\SkripsiController;
 Route::get('/', function () {
     return view('welcome');
 }); 
+
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::resource('elective-courses', ElectiveCourseController::class);
 Route::resource('students', StudentController::class);
