f95eae16004687e0d36e11c5d77d0cb9f6a4dcc6
AlIhsanAli <2300018178@webmail.uad.ac.id>
2026-06-23 16:46:18 +0700

    Implement lecturer management features: add index, edit, update, and store functionalities; create views for listing and editing lecturers; update routes and tests accordingly.

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ Route::get('/skripsi', [SkripsiController::class, 'index'])->name('skripsi.index
 Route::get('/skripsi/create', [SkripsiController::class, 'create'])->name('skripsi.create');
 Route::post('/skripsi', [SkripsiController::class, 'store'])->name('skripsi.store');
 Route::put('/skripsi/{id}/update-status', [SkripsiController::class, 'updateStatus'])->name('skripsi.updateStatus');
+Route::get('lecturers', [LecturerController::class, 'index'])->name('lecturers.index');
+Route::get('lecturers/{id}/edit', [LecturerController::class, 'edit'])->name('lecturers.edit');
+Route::put('lecturers/{id}', [LecturerController::class, 'update'])->name('lecturers.update');
 Route::post('lecturers', [LecturerController::class, 'store'])->name('lecturers.store');
 Route::delete('lecturers/{id}', [LecturerController::class, 'destroy'])->name('lecturers.destroy');
