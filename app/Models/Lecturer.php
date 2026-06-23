24181bc29e164b10f7a598cd4bf0e9ab0a2b14f1
Girhantri <girhantri.2005@gmail.com>
2026-06-23 11:54:21 +0700

    feat: implement skripsi management system with CRUD operations and seeder data

--- app/Models/Lecturer.php
+++ app/Models/Lecturer.php
@@ -24,7 +24,6 @@ class Lecturer extends Model
         'name',
         'email',
         'expertise',
-        'status',
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ElectiveCourseController;
+use App\Http\Controllers\SkripsiController;
 
 Route::get('/', function () {
     return view('welcome');
-}); 
+});
+
 Route::resource('elective-courses', ElectiveCourseController::class);
+
+Route::get('/skripsi', [SkripsiController::class, 'index'])->name('skripsi.index');
+Route::get('/skripsi/create', [SkripsiController::class, 'create'])->name('skripsi.create');
+Route::post('/skripsi', [SkripsiController::class, 'store'])->name('skripsi.store');
+Route::put('/skripsi/{id}/update-status', [SkripsiController::class, 'updateStatus'])->name('skripsi.updateStatus');
