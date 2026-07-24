a04ffb86354335ab7b0bc4a8ad229d95b24e0484
FegterId <fegasbima@gmail.com>
2026-07-24 10:20:44 +0700

    Menambahkan notifikasi dan relasi fix

--- database/migrations/2026_06_09_120608_create_students_table.php
+++ database/migrations/2026_06_09_120608_create_students_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
             $table->string('student_id')->unique();
             $table->string('name');
             $table->string('email')->unique();
